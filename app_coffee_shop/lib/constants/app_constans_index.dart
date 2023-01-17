8c16b2dc9ea0fe2f5a2a7cf0e9c5721329c02b32
Piotr Hariuc <acvatoris@gmail.com>
2023-01-18 00:10:33 +0200

    +

--- app_coffee_shop/lib/constants/app_constans_index.dart
+++ app_coffee_shop/lib/constants/app_constans_index.dart
@@ -1,6 +1,7 @@
 library app_constants_index;
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 part 'resources/color_manager.dart';
 
@@ -9,3 +10,5 @@ part 'resources/font_manager.dart';
 part 'resources/styles_manager.dart';
 
 part 'resources/values_manager.dart';
+
+part 'resources/assets_manager.dart';
